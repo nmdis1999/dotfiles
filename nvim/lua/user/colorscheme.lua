f9425461be314d467f3c2256d63d915aa5f62f8e
Iti Shree <itishree1999@gmail.com>
2023-08-05 22:09:29 +0100

    updated neovim configuration with keymaps.

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "gruvbox"
+
+vim.o.background = "dark"  -- Set background to dark mode
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
+
